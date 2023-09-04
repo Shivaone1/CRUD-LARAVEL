f24cb3690190feeb66a67572d617adb36ba887c8
Shiva <yesmyhomepoint@gmail.com>
2023-09-04 14:10:32 +0530

    Crud Laravel

---
+++ resources/views/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Hello Laravel Shiva</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Close Styles -->
+</head>
+<body class="antialiased">
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <h1>Hello Shiva</h1>
+    </div>
+</body>
+
+</html>
